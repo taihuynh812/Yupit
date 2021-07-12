fa08a59d65104575399ea8d2f170829b1fa85f78
taihuynh812 <taihuynh@revlisoft.com>
2021-07-12 15:00:03 -0400

    added profile dropdown menu

--- frontend/components/nav_bar/navbar_greeting.jsx
+++ frontend/components/nav_bar/navbar_greeting.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserAlt, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import * as RegFontAwesome from '@fortawesome/free-regular-svg-icons'
 
 class NavbarGreeting extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            open: false
+        }
+
+        this.toggleMenu = this.toggleMenu.bind(this)
+    }
+
+    toggleMenu(){
+        const open = this.state.open
+        this.setState({open: !open})
+    }
+
     greetingUser(){
+        const {open} = this.state
         return (
-            <div className="navbar-loggedin">
-                <h3 className='navbar-greeting-user'>Hello, {this.props.currentUser.firstname}</h3>
-                <button className="navbar-logout-button" onClick={this.props.logout}>Log Out</button>
+            <div className="navbar-loggedin" >
+                <div className='navbar-greeting-user-container' onClick={this.toggleMenu}>
+                    <FontAwesomeIcon icon={faUserAlt}/>
+                    <div className={open ? "drop-down-open" : "drop-down-close"} >
+                        <div className='drop-down-container'>
+                            <div className="user-dropdown-about-me">
+                                <div><FontAwesomeIcon icon={RegFontAwesome.faUserCircle} /></div>
+                                <div>About me</div> 
+                            </div>
+                            
+                            <div className="user-dropdown-log-out" onClick={this.props.logout}>
+                                <div><FontAwesomeIcon icon={faSignOutAlt} /></div>
+                                <div>Log Out</div> 
+                            </div> 
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
