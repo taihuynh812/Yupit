f4621fe984ae767563e8c29a7e51e0fafd63c3f3
taihuynh812 <taihuynh@revlisoft.com>
2021-07-13 15:47:13 -0400

    completed styling for review and show forms

--- frontend/components/review/review_index/review_item.jsx
+++ frontend/components/review/review_index/review_item.jsx
@@ -8,9 +8,11 @@ class ReviewItem extends React.Component{
 
         if (currentUser !== undefined && currentUser.id === review.user_id){
             return(
-                <div className="edit-delete-button">
-                    <Link to={`/businesses/${review.business_id}/reviews/${review.id}/update`}><button>Update</button></Link>
-                    <button 
+                <div className="edit-delete">
+                    <Link to={`/businesses/${review.business_id}/reviews/${review.id}/update`}>
+                        <button className="edit-delete-button">Update</button>
+                    </Link>
+                    <button className="edit-delete-button"
                         onClick={() => this.props.deleteReview(review.business_id, review.id)
                             .then(() => window.location.reload())}>
                         Delete
@@ -24,10 +26,17 @@ class ReviewItem extends React.Component{
         const {review, star} = this.props
         return(
             <div className='review-item-container'>
-                <div className='reviewer-name'>{review.user.username}</div>
+
+                <div className='reviewer-header'>
+                    <div className='reivewer'>
+                        <div><img className="user-icon" src="https://lh3.googleusercontent.com/pw/AM-JKLW4xJ1IDlinnfurjwqDmzDDcynW4aHH9XlMRpvL1SpGvoUwfYEv-31VVzFnJ4H8Xq-dcbPSM_STua6NS_lWjbTY-3N5sQJOw04bv6zRnqZdZvhqvWiG1LkM6RZFidU-UgpCODcPaoIZsfAMCOBp6kFU=s640-no?authuser=0"/></div>
+                        <div className='reviewer-name'>{review.user.username}</div>
+                    </div>
+                    <div className='reviewer-edit-delete'>{this.editDeleteButton()}</div>
+                </div>
+
                 <div className='review-date'>{star(review.rating)} {review.created_at}</div>
                 <div className='review-description'>{review.description}</div>
-                {this.editDeleteButton()}
             </div>
         )
     }
